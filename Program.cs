0452ff2178f31a941e4c9950e1ad8aae096e02f7
Danilo Costa <dandan_costa2222@outlook.com>
2025-03-17 10:04:57 -0300

    codigo do exercicio

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿string nome;  
+string sobrenome;
+
+
+Console.Write("Digiteo primeiro nome: ");
+nome = Console.ReadLine()!;
+
+Console.Write("Digite o sobrenome: ");
+sobrenome = Console.ReadLine()!;
+
+Console.WriteLine($"Nome completo: {nome} {sobrenome}");
+Console.WriteLine($"Nome de catálogo: {sobrenome.ToUpper()}, {nome}");
+
+
+
+
+
+
